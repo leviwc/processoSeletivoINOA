904c77d3cf4375d2b0d576517a1e61e8f0115d4d
leviwc <leviwc3@hotmail.com>
2021-03-10 16:43:37 -0300

    dotnet console c# program

---
+++ processoINOA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace processoINOA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
