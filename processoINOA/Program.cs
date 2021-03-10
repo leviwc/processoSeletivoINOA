a554bb0dc815a9e508418883d74aa11c9de011d2
leviwc <leviwc3@hotmail.com>
2021-03-10 20:33:24 -0300

    enviando email

--- processoINOA/Program.cs
+++ processoINOA/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
 namespace processoINOA
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+        public void SendEmail(bool buyStock)
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    UseDefaultCredentials = false,
+                    Port = 587,
+                    Credentials = new NetworkCredential("inoaprocessoseletivo@gmail.com", "inoa12345"),
+                    EnableSsl = true,
+                };
+                smtpClient.Send("inoaprocessoseletivo@gmail.com", "leviwc3@hotmail.com", "Alerta de preço da ação", (buyStock ? "Compre a ação" : "Venda a ação"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
+
 }
