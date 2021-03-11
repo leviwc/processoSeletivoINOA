43025e890a50c8272cb2647fd601433a29838f0f
leviwc <leviwc3@hotmail.com>
2021-03-10 21:54:17 -0300

    pegar informações de email do app.config

--- processoINOA/Program.cs
+++ processoINOA/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Mail;
 using System.Configuration;
+using System.Collections.Specialized;
 namespace processoINOA
 {
     class Program
@@ -10,18 +11,21 @@ namespace processoINOA
         {
 
         }
-        public void SendEmail(bool buyStock)
+        static void SendEmail(bool buyStock)
         {
             try
             {
+                string emailDeEnvio = ConfigurationManager.AppSettings.Get("emailDeEnvio");
+                string senhaDeEnvio = ConfigurationManager.AppSettings.Get("senhaDeEnvio");
+                string emailRecipiente = ConfigurationManager.AppSettings.Get("emailRecipiente");
                 var smtpClient = new SmtpClient("smtp.gmail.com")
                 {
                     UseDefaultCredentials = false,
                     Port = 587,
-                    Credentials = new NetworkCredential("inoaprocessoseletivo@gmail.com", "inoa12345"),
+                    Credentials = new NetworkCredential(emailDeEnvio, senhaDeEnvio),
                     EnableSsl = true,
                 };
-                smtpClient.Send("inoaprocessoseletivo@gmail.com", "leviwc3@hotmail.com", "Alerta de preço da ação", (buyStock ? "Compre a ação" : "Venda a ação"));
+                smtpClient.Send(emailDeEnvio, emailRecipiente, "Alerta de preço da ação", (buyStock ? "Compre a ação" : "Venda a ação"));
             }
             catch (Exception ex)
             {
