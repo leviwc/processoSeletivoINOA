eaad9e9d3121a29705a405108fd90e3e49823c58
leviwc <leviwc3@hotmail.com>
2021-03-12 18:14:41 -0300

    Usando YahooFinanceApi para pegar o preço da ação

--- processoINOA/Program.cs
+++ processoINOA/Program.cs
@@ -2,16 +2,48 @@
 using System.Net;
 using System.Net.Mail;
 using System.Configuration;
-using System.Collections.Specialized;
+using YahooFinanceApi;
+using System.Threading.Tasks;
+
 namespace processoINOA
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-
+            string stockName = args[1] + ".SA";
+            decimal redLine = Convert.ToDecimal(args[2]), blueLine = Convert.ToDecimal(args[3]);
+            int previusStateOfShare = 0; // -1 is bellow red line, 0 is in the middle and 1 is above blue line
+            while (true)
+            {
+                try
+                {
+                    var securities = await Yahoo.Symbols(stockName).Fields(Field.Symbol, Field.RegularMarketPrice, Field.FiftyTwoWeekHigh).QueryAsync();
+                    var stock = securities[stockName];
+                    decimal price = Convert.ToDecimal(stock.RegularMarketPrice);
+                    if (price <= redLine && previusStateOfShare != -1)
+                    {
+                        previusStateOfShare = -1;
+                        SendEmail(true, stockName);
+                    }
+                    else if (price >= blueLine && previusStateOfShare != 1)
+                    {
+                        previusStateOfShare = 1;
+                        SendEmail(false, stockName);
+                    }
+                    else if (price > redLine && price < blueLine)
+                    {
+                        previusStateOfShare = 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
         }
-        static void SendEmail(bool buyStock)
+        static void SendEmail(bool buyStock, string stockName)
         {
             try
             {
@@ -25,7 +57,7 @@ namespace processoINOA
                     Credentials = new NetworkCredential(emailDeEnvio, senhaDeEnvio),
                     EnableSsl = true,
                 };
-                smtpClient.Send(emailDeEnvio, emailRecipiente, "Alerta de preço da ação", (buyStock ? "Compre a ação" : "Venda a ação"));
+                smtpClient.Send(emailDeEnvio, emailRecipiente, "Alerta de preço da ação", (buyStock ? "Compre a ação " : "Venda a ação ") + stockName);
             }
             catch (Exception ex)
             {
