1851d7721a5f518a05073096d66b58b4bdf5ecac
leviwc <leviwc3@hotmail.com>
2021-03-12 21:49:52 -0300

    adicionando mais de uma ação a ser monitorada

--- processoINOA/Program.cs
+++ processoINOA/Program.cs
@@ -8,28 +8,41 @@ using System.Threading.Tasks;
 namespace processoINOA {
   class Program {
     static async Task Main(string[] args) {
-
-      string stockName = args[1] + ".SA";
-      decimal redLine = Convert.ToDecimal(args[2]), blueLine = Convert.ToDecimal(args[3]);
-      int previusStateOfShare = 0; // -1 is bellow red line, 0 is in the middle and 1 is above blue line
+      int quantityOfStocks = (args.Length - 1) / 3;
+      Stock[] stocks = new Stock[quantityOfStocks];
+      for (int i = 0; i < quantityOfStocks; i++) stocks[i] = new Stock();
+      int[] previusStateOfShares = new int[quantityOfStocks];// -1 is bellow red line, 0 is in the middle and 1 is above blue line
+      for (int i = 1, j = 0; i < args.Length; i += 3, j++) {
+        previusStateOfShares[j] = 0;
+        stocks[j].stockName = args[i] + ".SA";
+        stocks[j].redLine = Convert.ToDecimal(args[i + 1]);
+        stocks[j].blueLine = Convert.ToDecimal(args[i + 2]);
+        if (stocks[j].redLine >= stocks[j].blueLine) {
+          Console.Write("Invalid input, the redLine is above or equal the blueLine of stock " + stocks[j].stockName + '\n');
+          return;
+        }
+      }
       while (true) {
         try {
-          var securities = await Yahoo.Symbols(stockName).Fields(Field.Symbol, Field.RegularMarketPrice, Field.FiftyTwoWeekHigh).QueryAsync();
-          var stock = securities[stockName];
-          decimal price = Convert.ToDecimal(stock.RegularMarketPrice);
-          if (price <= redLine && previusStateOfShare != -1) {
-            previusStateOfShare = -1;
-            SendEmail(true, stockName);
-          } else if (price >= blueLine && previusStateOfShare != 1) {
-            previusStateOfShare = 1;
-            SendEmail(false, stockName);
-          } else if (price > redLine && price < blueLine) {
-            previusStateOfShare = 0;
+          for (int i = 0; i < quantityOfStocks; i++) {
+            var securities = await Yahoo.Symbols(stocks[i].stockName).Fields(Field.Symbol, Field.RegularMarketPrice, Field.FiftyTwoWeekHigh).QueryAsync();
+            var stock = securities[stocks[i].stockName];
+            decimal price = Convert.ToDecimal(stock.RegularMarketPrice);
+            if (price <= stocks[i].redLine && previusStateOfShares[i] != -1) {
+              previusStateOfShares[i] = -1;
+              SendEmail(true, stocks[i].stockName);
+            } else if (price >= stocks[i].blueLine && previusStateOfShares[i] != 1) {
+              previusStateOfShares[i] = 1;
+              SendEmail(false, stocks[i].stockName);
+            } else if (price > stocks[i].redLine && price < stocks[i].blueLine) {
+              previusStateOfShares[i] = 0;
+            }
           }
+
         } catch (Exception ex) {
           Console.WriteLine(ex.Message);
         }
-        System.Threading.Thread.Sleep(1000);
+        System.Threading.Thread.Sleep(2000);
       }
     }
     static void SendEmail(bool buyStock, string stockName) {
@@ -49,5 +62,10 @@ namespace processoINOA {
       }
     }
   }
-
+  public class Stock {
+    public string stockName;
+    public decimal redLine;
+    public decimal blueLine;
+    public Stock() { }
+  }
 }
